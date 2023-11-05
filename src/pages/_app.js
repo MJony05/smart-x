4a483004ca0756c9c6b2b6a7472e051dd160c2a6
Jonibek Munirov <94430009+MJony05@users.noreply.github.com>
2023-11-05 10:51:24 +0500

    favicon updates

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,13 @@
 import ParticlesComponent from '@/components/Particles';
 import '@/styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/x-logo.png" />
+      </Head>
       <Component {...pageProps} />
       <ParticlesComponent />
     </>

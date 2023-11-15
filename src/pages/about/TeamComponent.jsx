1dac095072a6d732dd4eff83f9ee958c120cf5c7
Jonibek Munirov <94430009+MJony05@users.noreply.github.com>
2023-11-15 10:40:16 +0500

    bug fixed

--- src/pages/about/TeamComponent.jsx
+++ src/pages/about/TeamComponent.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import CardItem from './CardItem';
 import styles from './about.module.css';
-const TeamComponent = ({ data }) => {
-  const members = [...data];
+const TeamComponent = (props) => {
+  const { data } = props;
   return (
     <div className={styles.teamContainer}>
       <h1 className={styles.title}>Bizning Jamoa</h1>
       <div className={styles.teamContent}>
-        {members?.length !== 0
-          ? members?.map((item) => {
+        {data?.length !== 0
+          ? data?.map((item) => {
               return (
                 <CardItem
                   key={item.id}

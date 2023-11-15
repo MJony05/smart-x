27900cb5bf7fc366c2a7433c14f920d6dd3b5db1
Jonibek Munirov <94430009+MJony05@users.noreply.github.com>
2023-11-15 10:34:31 +0500

    done

--- src/pages/about/TeamComponent.jsx
+++ src/pages/about/TeamComponent.jsx
@@ -2,21 +2,24 @@ import React from 'react';
 import CardItem from './CardItem';
 import styles from './about.module.css';
 const TeamComponent = ({ data }) => {
+  const members = [...data];
   return (
     <div className={styles.teamContainer}>
       <h1 className={styles.title}>Bizning Jamoa</h1>
       <div className={styles.teamContent}>
-        {data.map((item) => {
-          return (
-            <CardItem
-              key={item.id}
-              name={item.name}
-              position={item.position}
-              image={item.image}
-              quote={item.quote}
-            />
-          );
-        })}
+        {members?.length !== 0
+          ? members?.map((item) => {
+              return (
+                <CardItem
+                  key={item.id}
+                  name={item.name}
+                  position={item.position}
+                  image={item.image}
+                  quote={item.quote}
+                />
+              );
+            })
+          : 'Loading...'}
       </div>
     </div>
   );

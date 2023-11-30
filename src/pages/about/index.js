2d9f7e284633c9f74556e38c29360dbc00759742
Jonibek Munirov <94430009+MJony05@users.noreply.github.com>
2023-11-30 17:22:40 +0500

    done

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,44 +1,42 @@
 import React from 'react';
 import Navbar from './Navbar';
 import styles from './about.module.css';
-import TeamComponent from './TeamComponent';
 import CourseComponent from './CourseComponent';
-const data = [
-  {
-    id: 1,
-    name: 'Jonibek Munirov',
-    position: 'Web Developer',
-    image: '/jony.jpg',
-    quote: 'Consistency is the key to success',
-  },
-  {
-    id: 2,
-    name: 'Javohir Hakimov',
-    position: 'Founder | Coach',
-    image: '/javoxir.jpg',
-    quote: 'TiT for TaT!',
-  },
-  {
-    id: 3,
-    name: 'Muxiddin Alisherov',
-    position: 'Media Manager',
-    image: '/pepe.jpg',
-    quote: 'Professionalism is not enough',
-  },
-  {
-    id: 4,
-    name: 'Shoxrux Sharipov',
-    position: 'Mobile Developer',
-    image: '/shox.jpg',
-    quote: 'Well done is better than well said',
-  },
-];
+// const data = [
+//   {
+//     id: 1,
+//     name: 'Jonibek Munirov',
+//     position: 'Web Developer',
+//     image: '/jony.jpg',
+//     quote: 'Consistency is the key to success',
+//   },
+//   {
+//     id: 2,
+//     name: 'Javohir Hakimov',
+//     position: 'Founder | Coach',
+//     image: '/javoxir.jpg',
+//     quote: 'TiT for TaT!',
+//   },
+//   {
+//     id: 3,
+//     name: 'Muxiddin Alisherov',
+//     position: 'Media Manager',
+//     image: '/pepe.jpg',
+//     quote: 'Professionalism is not enough',
+//   },
+//   {
+//     id: 4,
+//     name: 'Shoxrux Sharipov',
+//     position: 'Mobile Developer',
+//     image: '/shox.jpg',
+//     quote: 'Well done is better than well said',
+//   },
+// ];
 const About = () => {
   return (
     <div className={styles.aboutPage}>
       <Navbar />
       <CourseComponent />
-      <TeamComponent data={data} />
     </div>
   );
 };
